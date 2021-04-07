12c5b1e096339251bb65be813bbcd6db339c6dea
mayankkumawat <mayankkumawat015@gmail.com>
2021-04-07 13:06:32 +0530

    Basic UI Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a href="#" className="navbar-brand">Navbar</a>
+
+          <ul className="nav">
+            <li className="nav-item">
+              <button className="btn btn-primary">Add Todo</button>
+            </li>
+          </ul>
+
+        </div>
+
+      </nav>
+
+      <div className="container-fluid">
+        <div className="row">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Meeting
+              <span className="badge bg-danger rounded-pill">&times;</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">Water</li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">Exercise</li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">Book Reading</li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">Programming</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
